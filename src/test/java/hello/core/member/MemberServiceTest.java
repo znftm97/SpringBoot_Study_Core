dcaa1c96ff455126b74ee319ebe1cd8ed7439574
jihoon <znftm97@naver.com>
2020-10-26 21:32:41 +0900

    MemberService Test with Junit

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join(){
+        //give
+        Member member = new Member(1L, "jh", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
