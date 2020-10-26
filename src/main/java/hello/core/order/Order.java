f98a1ff80fc1efb265f206b8ffa7f20f268e36ca
jihoon <znftm97@naver.com>
2020-10-26 23:45:32 +0900

    order,discount development && orderService Test with Junit

---
+++ src/main/java/hello/core/order/Order.java
@@ -0,0 +1,62 @@
+package hello.core.order;
+
+public class Order {
+
+    private Long memberID; // 회원 id
+    private String itemName; // 상품 이름
+    private int itemPrice; // 상품 금액
+    private int discountPrice; // 할인 금액
+
+    public Order(Long memberID, String itemName, int itemPrice, int discountPrice) {
+        this.memberID = memberID;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+    }
+
+    public int calculaterPrice(){
+        return itemPrice - discountPrice;
+    }
+
+    public Long getMemberID() {
+        return memberID;
+    }
+
+    public void setMemberID(Long memberID) {
+        this.memberID = memberID;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(int discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "memberID=" + memberID +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", discountPrice=" + discountPrice +
+                '}';
+    }
+}
